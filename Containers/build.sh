92853df3d29835169a2447fe33dcc3a0f46cfd28
Chris Love <christopherlove68@gmail.com>
2024-02-14 19:44:20 -0800

    Local updates to RHEL8

--- Containers/build.sh
+++ Containers/build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-cp ../build/bin/thread threadtest
-cp ../build/bin/thread threadtestdebug
 
-cd threadtest
+cd threadtest || return
 docker build -t threadtest:latest -t fir.love.io:3005/threadtest:latest .
 docker push fir.love.io:3005/threadtest:latest
 
 cd ..
-
-cd threadtestdebug
-docker build -t threadtestdebug:latest .
